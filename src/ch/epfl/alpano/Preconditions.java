a16b66c955d5591c3c6200e4c465edd1c26ba9f9
AndreaScalisi <andrea.scalisi@epfl.ch>
2017-02-24 13:57:25 +0100

    Final

--- src/ch/epfl/alpano/Preconditions.java
+++ src/ch/epfl/alpano/Preconditions.java
@@ -2,20 +2,18 @@ package ch.epfl.alpano;
 
 public interface Preconditions {
     /**
-     * 
-     * @param b if b is false an Illegal Argument Exception is thrown
+     * if b is false an Illegal Argument Exception is thrown
+     * @param b a boolean
      */
-    public static void checkArgument(boolean b)
-    {
+    public static void checkArgument(boolean b){
         if(!b) throw new IllegalArgumentException();
     }
     /**
-     * 
-     * @param b if b is false an Illegal Argument Exception is thrown
-     * @param msg the message associated to the exception
+     * if b is false an Illegal Argument Exception is thrown
+     * @param b a boolean 
+     * @param msg, the message associated to the exception
      */
-    public static void checkArgument(boolean b, String msg)
-    {
+    public static void checkArgument(boolean b, String msg){
         if(!b) throw new IllegalArgumentException(msg);
     }
 
