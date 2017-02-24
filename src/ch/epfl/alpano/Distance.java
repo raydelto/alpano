2daf14e33fb972b172f44c6ff3d275dbb2cb2296
AndreaScalisi <andrea.scalisi@epfl.ch>
2017-02-24 13:59:25 +0100

    Distance Final

--- src/ch/epfl/alpano/Distance.java
+++ src/ch/epfl/alpano/Distance.java
@@ -1,24 +1,22 @@
 package ch.epfl.alpano;
 
 public interface Distance {
-    public static double EARTH_RADIUS=6371000;
+    public static final double EARTH_RADIUS=6371000;
     
     /**
-     * 
+     * This method converts a distance in meters at the earth surface to the corresponding angle in radians
      * @param distanceInMeters distance to be converted in radians
      * @return the distance measured in radians
      */
-    public static double toRadians(double distanceInMeters)
-    {
+    public static double toRadians(double distanceInMeters){
         return distanceInMeters/EARTH_RADIUS;
     }
     /**
-     * 
+     * This method converts an angle in radians to a distance in meters at the earth surface
      * @param distanceInRadians radians to be converted to meters
      * @return the distance measured in meters
      */
-    public static double toMeters(double distanceInRadians)
-    {
+    public static double toMeters(double distanceInRadians){
         return distanceInRadians*EARTH_RADIUS;
     }
 
