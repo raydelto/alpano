ca0e4567f516d30b261e79a12e278b61ceaac543
byrek3d <gerald.sula@epfl.ch>
2017-02-24 18:38:52 +0100

    Interval1D
    with some comments in albanian

---
+++ src/ch/epfl/alpano/Interval1D.java
@@ -0,0 +1,144 @@
+package ch.epfl.alpano;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+public final class Interval1D {
+    private  int includedFrom,  includedTo;
+    
+    /**
+     * Creates a uni-dimensional interval, throws IllegalArgumentException is includeTo is strictly smaller than Includedfrom
+     * @param includedFrom lower bound of the interval
+     * @param includedTo upper bound of the interval
+     */
+    public Interval1D(int includedFrom, int includedTo) 
+    { 
+        Preconditions.checkArgument(!(includedTo<includedFrom));
+        this.includedFrom=includedFrom;
+        this.includedTo=includedTo;
+    }
+    
+    /**
+     * 
+     * @return returns the lower bound of the interval
+     */
+    public int includedFrom()
+    {
+        return includedFrom;
+    }
+    
+    /**
+     * 
+     * @return returns the upper bound of the interval
+     */
+    public int includedTo() 
+    {
+        return includedTo;
+    }
+    
+    /**
+     * size of the interval
+     * @return eturns the size of the interval
+     */
+    public int size()
+    {
+        return includedTo-includedFrom;
+    }
+    
+    /**
+     * checks if the element v is contained in the interval
+     * @param v the element to be checked
+     * @return returns true if the element is contained in the interval
+     */
+    public boolean contains(int v)
+    {
+        return (v>=includedFrom()&&v<=includedTo());
+    }
+    
+    /**
+     * Calculates the size of a intersection between two intervals, throws IllegalArgumentException if the two don't intersect
+     * @param that the second interval
+     * @return the size of the intersection
+     */
+    public int sizeOfIntersectionWith(Interval1D that)
+    {
+        if(!this.isUnionableWith(that)) return 0;
+        int iArr[] = {this.includedTo(),that.includedFrom(),that.includedTo(),this.includedFrom()};
+        Arrays.sort(iArr);
+        return iArr[2]-iArr[1];
+        
+    }
+    
+    /**
+     * Checks if the bounding two intervals is a continuous interval
+     * @param i1 the first interval
+     * @param i2 the second interval
+     * @return
+     */
+    public boolean isUnionableWith(Interval1D that)//PYT: esht ok nqs e bojm ndryshe
+    {
+        return(this.includedFrom()<=that.includedTo()&&this.includedTo()>=that.includedFrom());
+        //return((this.size()+that.size()-this.sizeOfIntersectionWith(that))==this.boundingUnion(that).size());
+    }
+    
+    /**
+     * Bounds two intervals into one
+     * @param that the second interval
+     * @return a new interval that bounds two intervals into one
+     */
+    public Interval1D union(Interval1D that)
+    {
+        Preconditions.checkArgument(this.isUnionableWith(that));
+        int iArr[] = {this.includedTo(),that.includedFrom(),that.includedTo(),this.includedFrom()};
+        Arrays.sort(iArr);
+        return new Interval1D(iArr[0], iArr[3]);
+        
+    }
+    
+    /**
+     * Bounds inclusively (englobante) two intervals into one
+     * @param that the second interval
+     * @return a new interval that bounds two intervals into one
+     */
+    public Interval1D boundingUnion(Interval1D that)//Pyt per kte a esht ok
+    {
+        int low, up;
+        if(this.includedFrom()<=that.includedFrom()) 
+            {
+            low=this.includedFrom();
+            }
+        else low=that.includedFrom();
+        
+        if(this.includedTo()>=that.includedTo()) 
+        {
+        up=this.includedTo();
+        }
+        else up=that.includedTo();
+        
+        return new Interval1D(low,up);
+    }
+    
+    @Override
+    public boolean equals(Object thatO)
+    {
+        if(thatO==null)return false;
+        if(thatO.getClass()!=this.getClass())return false;
+        if(this.hashCode()!=thatO.hashCode()) return false;
+        Interval1D that= (Interval1D)thatO;
+        return(this.includedTo()==that.includedTo()&&this.includedFrom()==that.includedFrom());
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "["+this.includedFrom+".."+this.includedTo+"]";
+    }
+    
+    @Override
+    public int hashCode() {
+      return Objects.hash(includedFrom(), includedTo());
+    }
+    
+
+}
