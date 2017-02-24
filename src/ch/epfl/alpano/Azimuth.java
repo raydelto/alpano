05076672a5191cda6c1e8d0d8374c8533944a900
AndreaScalisi <andrea.scalisi@epfl.ch>
2017-02-24 14:32:10 +0100

    Test

--- src/ch/epfl/alpano/Azimuth.java
+++ src/ch/epfl/alpano/Azimuth.java
@@ -11,10 +11,14 @@ public interface Azimuth {
      * @return true if the angle is included between 0 and (excluding)2PI
      */
     public static boolean isCanonical(double azimuth) {
-        if (azimuth >= 0 && azimuth < Math2.PI2)
+        if (azimuth >= 0 && azimuth < Math2.PI2){
             return true;
-        else
+        }
+            
+        else{
             return false;
+        }
+            
     }
 
     /**
@@ -25,7 +29,6 @@ public interface Azimuth {
      */
     public static double canonicalize(double azimuth) {
         return Math.abs(Math2.floorMod(azimuth, Math2.PI2));
-
     }
 
     /**
@@ -37,9 +40,15 @@ public interface Azimuth {
      */
     public static double toMath(double azimuth) {
         Preconditions.checkArgument(isCanonical(azimuth));
-        if (azimuth == 0)
+        if (azimuth == 0){
             return azimuth;
-        return Math2.PI2 - azimuth;
+        }
+        
+        else{
+            return Math2.PI2 - azimuth;
+        }
+            
+        
     }
 
     /**
@@ -52,9 +61,15 @@ public interface Azimuth {
     public static double fromMath(double azimuth) {
 
         Preconditions.checkArgument(isCanonical(azimuth));
-        if (azimuth == 0)
+        if (azimuth == 0){
+            
             return azimuth;
-        return Math2.PI2 - azimuth;
+        }
+        
+        else{
+            return Math2.PI2 - azimuth;
+        }
+         
     }
 
     /**
@@ -66,21 +81,19 @@ public interface Azimuth {
      * @param w The String for the west direction
      * @return
      */
-    public static String toOctantString(double azimuth, String n, String e,
-            String s, String w) {
-        double mathAzimuth = toMath(azimuth);
+    public static String toOctantString(double azimuth, String n, String e, String s, String w) {
+        double mathAzimuth = azimuth;
+       
         String direction = "";
-        if ((mathAzimuth >= 0 && mathAzimuth <= 3 * PI / 8)
-                || mathAzimuth >= (13 * PI / 8)) {
+        if ((mathAzimuth >= 0 && mathAzimuth <= 3 * PI / 8) || mathAzimuth >= (13 * PI / 8)) {
             direction += n;
         } else if (mathAzimuth >= (5 * PI / 8) && mathAzimuth <= 11 * PI / 8) {
             direction += s;
         }
         if (mathAzimuth >= PI / 8 && mathAzimuth <= 7 * PI / 8) {
-            direction += w;
-        } else if (mathAzimuth >= (9 * PI / 8)
-                && mathAzimuth <= (15 * PI / 8)) {
             direction += e;
+        } else if (mathAzimuth >= (9 * PI / 8) && mathAzimuth <= (15 * PI / 8)) {
+            direction += w;
         }
 
         System.out.println(direction);
