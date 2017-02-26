45c6c5349f4e465b02843987e81ece72f464f3d9
AndreaScalisi <andrea.scalisi@epfl.ch>
2017-02-26 18:35:18 +0100

    GeoPoint with annotations and Interval1D JunitTests

--- src/ch/epfl/alpano/GeoPoint.java
+++ src/ch/epfl/alpano/GeoPoint.java
@@ -13,6 +13,12 @@ public final class GeoPoint {
     private double longitude;
     private double latitude;
     
+    //Radians or degree ?
+    /**
+     * Creates a GeoPoint
+     * @param longitude, the longitude of the point in degrees
+     * @param latitude, the latitude of the point in degrees
+     */
     public GeoPoint(double longitude, double latitude){
         
         this.longitude = toRad(longitude);
@@ -24,36 +30,69 @@ public final class GeoPoint {
         
     }
     
+    /**
+     * 
+     * @return the longitude of the point in radians
+     */
     public double longitude(){
         return longitude;
     }
     
+    /**
+     * 
+     * @return the latitude of the point in radians
+     */
     public double latitude(){
         return latitude;
     }
     
+    //Précision ?
+    /**
+     * This method calculates a distance between two geopoints
+     * @param that, a geopoint
+     * @return the distance between the two geopoints, in meters
+     */
     public double distanceTo(GeoPoint that){
         double angle = 2*(asin(sqrt(haversin(this.latitude()-that.latitude())+cos(this.latitude())*cos(that.latitude())*haversin(this.longitude()-that.longitude()))));
         return Distance.toMeters(angle);
     }
     
     //Retourne en degrés ou en radians ?
+    /**
+     * Calculate the azimuth between two geopoints
+     * @param that, a geopoint
+     * @return the azimuth between the two geopoints, in degrees
+     */
     public double azimuthTo(GeoPoint that){
         
         double angle = atan2((sin(this.longitude()-that.longitude())*cos(that.latitude())),(cos(this.latitude())*sin(that.latitude()))-sin(this.latitude())*cos(that.latitude())*cos(this.longitude()-that.longitude()));
         return toDeg(Azimuth.fromMath(Azimuth.canonicalize(angle)));
     }
     
+    /**
+     * Redefinition of the method toString89 from Object
+     * @return a String that indicates the longitude and the latitude of the geopoint in degrees
+     */
     public String toString(){
         Locale l = null;
         String s = String.format(l, "(%.4f,%.4f)", toDeg(this.longitude()), toDeg(this.latitude()));
         return s;
     }
     
+    /**
+     * Converts an angle from radians to degrees
+     * @param angleInRad, the angle in radians to be converted
+     * @return the angle in degree
+     */
     public double toDeg(double angleInRad){
         return (angleInRad*180/PI);
     }
     
+    /**
+     * Converts an angle from degrees to radians
+     * @param angleInDeg, the angle in degrees to be converted
+     * @return the angle in radians
+     */
     public double toRad(double angleInDeg){
         return (angleInDeg*PI/180);
     }
---
+++ test/ch/epfl/alpano/Interval1DTest.java
@@ -0,0 +1,149 @@
+package ch.epfl.alpano;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Interval1DTest {
+
+    @Test (expected = IllegalArgumentException.class)
+    public void constructorThrowsException() {
+        Interval1D i = new Interval1D(2, 1);
+    }
+    
+    @Test
+    public void includedFromWorksWithSmallInterval(){
+        Interval1D i = new Interval1D(1, 1);
+        Interval1D i2 = new Interval1D(-2, -1);
+        assertEquals(1, i.includedFrom(), 0);
+        assertEquals(-2, i2.includedFrom(), 0);
+        
+    }
+    
+    @Test
+    public void includedToWorksWithSmallInterval(){
+        Interval1D i = new Interval1D(1, 1);
+        Interval1D i2 = new Interval1D(-2, -1);
+        assertEquals(1, i.includedTo(), 0);
+        assertEquals(-1, i2.includedTo(), 0);
+    }
+    
+    @Test
+    public void containsWorks(){
+        Interval1D i = new Interval1D(-5, 5);
+        assertTrue(i.contains(2));
+        assertFalse(i.contains(6));
+        assertTrue(i.contains(5));
+        
+    }
+    
+    @Test
+    public void containsWorksWithPoint(){
+        Interval1D i = new Interval1D(1, 1);
+        assertTrue(i.contains(1));        
+    }
+    
+    @Test
+    public void sizeWorks(){
+        Interval1D i = new Interval1D(0, 6);
+        assertEquals(7, i.size(), 0);        
+    }
+    
+    @Test
+    public void sizeWorksWithPoint(){
+        Interval1D i = new Interval1D(1, 1);
+        assertEquals(1, i.size(), 0);        
+    }
+    
+    @Test
+    public void sizeOfIntersectionWithWorks(){
+        Interval1D i = new Interval1D(-6, 4);
+        Interval1D i2 = new Interval1D(-9, 1);
+        assertEquals(8, i.sizeOfIntersectionWith(i2), 0);
+        
+        
+    }
+    
+    @Test
+    public void sizeOfIntersectionWithWorksWithEmptyIntersection(){
+        Interval1D i = new Interval1D(-2, 4);
+        Interval1D i2 = new Interval1D(5, 18);
+        assertEquals(0, i.sizeOfIntersectionWith(i2), 0);
+    }
+    
+    @Test
+    public void sizeOfIntersectionWithWorksWithIntersectionOfOne(){
+        Interval1D i = new Interval1D(-2, 4);
+        Interval1D i2 = new Interval1D(4, 18);
+        
+        assertEquals(1, i.sizeOfIntersectionWith(i2), 0);
+    }
+    
+    @Test
+    public void boundingUnionWorks(){
+        Interval1D i = new Interval1D(-2, 4);
+        Interval1D i2 = new Interval1D(2, 18);
+        Interval1D i3 = new Interval1D(-2, 18);
+        assertTrue(i3.equals(i.boundingUnion(i2)));
+        
+        
+    }
+    
+    @Test
+    public void boundingUnionWorksWithIntersectionOfOne(){
+        Interval1D i = new Interval1D(-2, 4);
+        Interval1D i2 = new Interval1D(4, 18);
+        Interval1D i3 = new Interval1D(-2, 18);
+        assertTrue(i3.equals(i.boundingUnion(i2)));
+    }
+    
+    @Test
+    public void isUnionableWithWorks(){
+        Interval1D i = new Interval1D(-2, 4);
+        Interval1D i2 = new Interval1D(4, 18);
+        Interval1D i3 = new Interval1D(-2, 6);
+        Interval1D i4 = new Interval1D(5, 18);
+        Interval1D i5 = new Interval1D(6, 7);
+        Interval1D i6 = new Interval1D(1, 1);
+        Interval1D i7 = new Interval1D(2, 2);
+       /* System.out.println(i.isUnionableWith(i2));
+        System.out.println(i.isUnionableWith(i3));
+        System.out.println(i.isUnionableWith(i4));
+        System.out.println(i.isUnionableWith(i5));
+        System.out.println(i6.isUnionableWith(i7));
+        */
+        assertTrue(i.isUnionableWith(i2));
+        assertTrue(i.isUnionableWith(i3));
+        assertTrue(i.isUnionableWith(i4));
+        assertFalse(i.isUnionableWith(i5));
+        assertTrue(i6.isUnionableWith(i7));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void unionThrowsException(){
+        
+        Interval1D i = new Interval1D(-2, 4);
+        Interval1D i2 = new Interval1D(7, 18);
+        i.union(i2);
+        
+    }
+    
+    @Test
+    public void unionWorks(){
+        Interval1D i = new Interval1D(-12, -4);
+        Interval1D i2 = new Interval1D(-5, 1);
+        Interval1D i3 = i.union(i2);
+        assertEquals(-12, i3.includedFrom(), 0);
+        assertEquals(1, i3.includedTo(), 0);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
