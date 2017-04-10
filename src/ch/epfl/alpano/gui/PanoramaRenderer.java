beefb5165b59e49af9c25693283f9bc260a29228
Unknown <gerald.sula@epfl.ch>
2017-04-10 14:01:39 +0200

    renderer

--- src/ch/epfl/alpano/gui/PanoramaRenderer.java
+++ src/ch/epfl/alpano/gui/PanoramaRenderer.java
@@ -1,13 +1,21 @@
 package ch.epfl.alpano.gui;
 
 import ch.epfl.alpano.Panorama;
-import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 public interface PanoramaRenderer {
     
     static WritableImage renderPanorama(Panorama pan, ImagePainter painter)
     {
-        return new WritableImage(pan, arg1)
+        WritableImage wi=new WritableImage(pan.parameters().width(), pan.parameters().height());
+        for(int i=0;i<pan.parameters().width();i++)
+        {
+            for(int j=0;j<pan.parameters().height();j++)
+            {
+                wi.getPixelWriter().setColor(i, j, painter.colorAt(i, j));
+            }
+        }
+        return wi;
+        
     }
 
 }
