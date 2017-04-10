b59093ea0eec04f923f208a467374eb3792b175f
AndreaScalisi <andrea.scalisi@epfl.ch>
2017-04-10 13:34:45 +0200

    ChannelPainter complete

--- src/ch/epfl/alpano/gui/ChannelPainter.java
+++ src/ch/epfl/alpano/gui/ChannelPainter.java
@@ -1,15 +1,52 @@
 package ch.epfl.alpano.gui;
 
-import ch.epfl.alpano.Panorama;
 import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+import java.util.function.DoubleUnaryOperator;
+
+import ch.epfl.alpano.Panorama;
+
 
 public interface ChannelPainter {
     
     public abstract float valueAt(int x, int y);
     
+    
     public static ChannelPainter maxDistanceToNeighbors(Panorama p){
         return  (x, y) ->  max(max(p.distanceAt(x-1, y), p.distanceAt(x, y-1)), max(p.distanceAt(x+1, y, 0), p.distanceAt(x, y+1, 0))) - p.distanceAt(x, y, 0);
     }
 
+    public default ChannelPainter add(float c){
+        return (x, y) -> (valueAt(x, y) + c);
+    }
     
+    public default ChannelPainter sub(float c){
+        return (x, y) -> (valueAt(x, y) - c);
+    }
+    
+    public default ChannelPainter mul(float c){
+        return (x, y) -> (valueAt(x, y)*c);
+    }
+    
+    public default ChannelPainter div(float c){
+        return (x, y) -> (valueAt(x, y)/c);
+    }
+    
+    public default ChannelPainter map(DoubleUnaryOperator d){
+        return (x, y) -> (float)d.applyAsDouble(valueAt(x, y));
+    }
+    
+    public default ChannelPainter inverted(){
+        return (x, y) -> (1 - valueAt(x, y));
+    }
+    
+    public default ChannelPainter clamped(){
+        return (x, y) -> max(0, min(valueAt(x, y), 1));
+        
+    }
+    
+    public default ChannelPainter cycling(){
+        return (x, y) -> valueAt(x, y)%1;
+    }
 }
